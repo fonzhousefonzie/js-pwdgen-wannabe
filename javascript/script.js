13ab4d8cfc42cd2d6c6c5b0140df9406e12eb83d
fonzhousefonzie <harth.fonz@gmail.com>
2022-05-17 13:38:32 +0200

    completed

---
+++ javascript/script.js
@@ -0,0 +1,7 @@
+const nomeUtente = prompt('Come ti chiami?');
+const cognomeUtente = prompt('Qual è il tuo cognome?');
+const coloreUtente = prompt('Qual è il tuo colore preferito?');
+
+const newPassword = nomeUtente + cognomeUtente + coloreUtente + '22';
+
+document.getElementById("newPassword").innerHTML = newPassword.toLowerCase() ;
